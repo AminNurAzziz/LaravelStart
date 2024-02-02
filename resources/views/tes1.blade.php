270dc2fec2c7e15a5e746d3a8c52833889ac22d0
AMIN NUR AZZIZ <2131710034@student.polinema.ac.id>
2024-02-03 03:06:45 +0700

    first commit

---
+++ resources/views/tes1.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tes1</title>
+</head>
+<body>
+    <h1>Hallo</h1>
+    <p>Ini adalah halaman tes1</p>
+    <p>Nilai adalah: {{ $name }}</p>
+</body>
+</html>
